2824aa89acb7893c1f9a2d9937922c045f0e2518
n8sty <ng269@cornell.edu>
2014-10-05 21:53:55 -0400

    add working links
    added working href links to the source data links

--- ui.R
+++ ui.R
@@ -44,9 +44,9 @@ shinyUI(fluidPage(
       showOutput('myChart2', 'leaflet'),
       p('Consider this a pre-alpha.'),
       p('The above is a geographic mapping of police shootings of civilians in the United States over the period defined at left. When viewing the data, note that placement of geo tags on the map are not precise because exact location information is not available in the data set. Furthermore, the location data (particularly discrepencies between county and city) have not been cleaned so some points are misplaced. To avoid overplotting of points, the latitude and longitude are jiggered randomly, which may sometimes result in a geo tag being placed in a location that makes little sense, like a pond. Click on a point for more information about a given shooting, and follow the links provided. To adjust the number of points displayed use the date range input provided to the left. Data for the police shootings comes from this Google Sheet: '),
-      a('http://goo.gl/zUdwkg'),
+      tags$a(href = 'http://goo.gl/zUdwkg', 'http://goo.gl/zUdwkg'),
       p('which is managed by Deadspin\'s Kyle Wagner. Geographic (latitude and longitude) information comes from Sujee Maniyam\'s blog:'),
-      a('http://sujee.net/tech/articles/geocoded/')
+      tags$a(href = 'http://sujee.net/tech/articles/geocoded/', 'http://sujee.net/tech/articles/geocoded/')
     )
   )
 ))
